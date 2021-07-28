c20d110531dc0a6a767e24d5a279533439be6dfe
Tamzid Ahmed <tamjidahmed958@gmail.com>
2021-07-29 00:24:37 +0600

    fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
 import {Grid, Paper} from "@material-ui/core";
 
-export default function MaterialNumberRating({start, end, current, itemPerGrid="auto", backgroundColor = "#FF2D5526", textColor = "#780017",
+export default function MaterialNumberRating({start, end, current, itemPerGrid= false, backgroundColor = "#FF2D5526", textColor = "#780017",
                                                  onActiveBackgroundColor = "#FF2D55", onActiveTextColor = "#780017", padding = [15, 15, 0, 0], spacing = 1,
                                                  borderRadius = 0, fontWeight = "bolder", fontSize = 20, mouseEnteredOn, clickedOn}) {
 
