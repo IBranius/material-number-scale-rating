c8d6f8d16439073e8d0638753b0a74d8b6e461c6
Tamzid Ahmed <tamjidahmed958@gmail.com>
2021-07-28 17:26:24 +0600

    fix

--- src/index.js
+++ src/index.js
@@ -5,14 +5,21 @@ import {number} from "prop-types";
 export default function MaterialNumberRating({ start, end, backgroundColor = "#FFFFFF", textColor = "#000000",
 onActiveBackgroundColor = "#2196f3", onActiveTextColor = "#FFFFFF", padding = 2, callBack}) {
     const [current_index, setIndex] = useState(0);
-    const array = (startIndex , endIndex) => Array<number>(endIndex - startIndex + 1).fill(0).map((_, idx) => startIndex + idx);
+
+    const numberRatingArray = (startIndex , endIndex) => {
+       let array = [];
+        for (let i = startIndex; i < endIndex; i++) {
+            array.push(i);
+        }
+        return array;
+    };
 
     return (
         <div style={{
             flexGrow: 1,
         }}>
             <Grid container spacing={1}>
-                {array(start, end).map((a, index) => (
+                {numberRatingArray(start, end).map((a, index) => (
                     <Grid item sm key={index}>
                         {(current_index > index) ? (
                             <Paper
